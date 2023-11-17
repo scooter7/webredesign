0af72d139253dc7406a8dac73d2ebb1c063e7b59
scooter7 <scooter.vineburgh@gmail.com>
2023-11-16 23:24:11 -0600

    Update webredesign.py

--- webredesign.py
+++ webredesign.py
@@ -1,47 +1,46 @@
 import streamlit as st
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 import openai
 import base64
-
-def chunk_string(string, length):
-    for i in range(0, len(string), length):
-        yield string[i:i + length]
-
-def get_modifications(chunk, instructions, api_key):
-    openai.api_key = api_key
-    prompt = f"Modify this HTML chunk based on these instructions:\nInstructions: {instructions}\nHTML Chunk:\n{chunk}"
-    response = openai.completions.create(
-        model="text-davinci-003",
-        prompt=prompt,
-        max_tokens=1000
-    )
-    return response.choices[0].text.strip()
 
 st.title("Web Page Modifier")
 
 url = st.text_input("Enter the URL of the web page:")
-modification_request = st.text_area("Describe the modifications you want in natural language:")
+modification_request = st.text_area("Describe the modifications you want:")
 
-if url and modification_request:
+if url:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    full_html_content = soup.prettify()
-    st.text_area("Original HTML", full_html_content, height=300)
+    st.text_area("Source Code", soup.prettify(), height=300)
 
-    # Retrieve the OpenAI API key from Streamlit secrets
-    api_key = st.secrets["OPENAI_API_KEY"]
+if modification_request:
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
+    detailed_prompt = (
+        "Generate complete and functional HTML/CSS code for the following modification: "
+        f"'{modification_request}'."
+    )
+    response = openai.completions.create(
+        model="text-davinci-003",  # Replace with the correct GPT-4 model identifier
+        prompt=detailed_prompt,
+        max_tokens=150
+    )
 
-    html_chunks = list(chunk_string(full_html_content, 2000))
-    modified_html_chunks = []
+    generated_code = response.choices[0].text.strip()
+    st.text_area("Generated HTML/CSS", generated_code)
 
-    for chunk in html_chunks:
-        modified_chunk = get_modifications(chunk, modification_request, api_key)
-        modified_html_chunks.append(modified_chunk)
+    if soup:
+        if "<style>" in generated_code or "css" in modification_request.lower():
+            style_tag = soup.new_tag("style")
+            style_tag.string = generated_code
+            soup.head.append(style_tag)
+        else:
+            new_html = BeautifulSoup(generated_code, 'html.parser')
+            soup.body.insert(0, new_html)
 
-    final_modified_html = ''.join(modified_html_chunks)
-    st.text_area("Modified HTML", final_modified_html, height=300)
-    
-    b64 = base64.b64encode(final_modified_html.encode()).decode()
-    href = f'<a href="data:file/html;base64,{b64}" download="modified_page.html">Download Modified HTML</a>'
-    st.markdown(href, unsafe_allow_html=True)
+        modified_html = soup.prettify()
+        st.text_area("Modified Source Code", modified_html, height=300)
+
+        b64 = base64.b64encode(modified_html.encode()).decode()
+        href = f'<a href="data:file/html;base64,{b64}" download="modified_page.html">Download Modified HTML</a>'
+        st.markdown(href, unsafe_allow_html=True)
