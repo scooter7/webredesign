c2d9d829496b9aa3cdccf5cd437649ded4a88874
scooter7 <scooter.vineburgh@gmail.com>
2023-11-16 19:26:05 -0600

    Update webredesign.py

--- webredesign.py
+++ webredesign.py
@@ -21,21 +21,21 @@ if modification_request:
     # Use OpenAI API for natural language processing
     openai.api_key = st.secrets["OPENAI_API_KEY"]
     response = openai.chat.completions.create(
-        model="text-davinci-003",  # Updated model name
+        model="text-davinci-003",
         prompt=f"Translate this into HTML/CSS: {modification_request}",
         max_tokens=150
     )
     generated_code = response.choices[0].text.strip()
     st.text_area("Generated Code", generated_code)
 
-        # Apply and display modifications (simplified example)
-        new_style = soup.new_tag('style')
-        new_style.string = generated_code
-        soup.head.append(new_style)
-        modified_html = soup.prettify()
-        st.text_area("Modified Source Code", modified_html, height=300)
+    # Apply and display modifications (simplified example)
+    new_style = soup.new_tag('style')
+    new_style.string = generated_code
+    soup.head.append(new_style)
+    modified_html = soup.prettify()
+    st.text_area("Modified Source Code", modified_html, height=300)
 
-        # Create downloadable file
-        b64 = base64.b64encode(modified_html.encode()).decode()
-        href = f'<a href="data:file/html;base64,{b64}" download="modified_page.html">Download Modified HTML</a>'
-        st.markdown(href, unsafe_allow_html=True)
+    # Create downloadable file
+    b64 = base64.b64encode(modified_html.encode()).decode()
+    href = f'<a href="data:file/html;base64,{b64}" download="modified_page.html">Download Modified HTML</a>'
+    st.markdown(href, unsafe_allow_html=True)
