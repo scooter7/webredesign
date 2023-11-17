4e0fc830309dab7cf2e78d0c550563c9e3378a25
scooter7 <scooter.vineburgh@gmail.com>
2023-11-16 21:46:33 -0600

    Update webredesign.py

--- webredesign.py
+++ webredesign.py
@@ -19,25 +19,24 @@ if fetch_button and url:
 
 instructions = st.text_area("Enter your modification instructions:", height=100)
 
-if instructions and html_content:
-    modify_button = st.button("Modify Webpage")
+modify_button = st.button("Modify Webpage")
 
-    if modify_button:
-        # Creating a prompt for OpenAI
-        prompt = f"Here is a webpage HTML source code:\n{html_content}\n\nMake the following changes to the HTML source code based on these instructions:\n{instructions}\n\nModified HTML:"
+if modify_button and instructions and html_content:
+    # Creating a prompt for OpenAI
+    prompt = f"Here is a webpage HTML source code:\n{html_content}\n\nMake the following changes to the HTML source code based on these instructions:\n{instructions}\n\nModified HTML:"
 
-        # Process instructions with OpenAI
-        response = openai.completions.create(
-            model="text-davinci-004", 
-            prompt=prompt, 
-            max_tokens=500
-        )
+    # Process instructions with OpenAI
+    response = openai.completions.create(
+        model="text-davinci-004", 
+        prompt=prompt, 
+        max_tokens=500
+    )
 
-        modified_html = response.choices[0].text.strip()
+    modified_html = response.choices[0].text.strip()
 
-        # Check if the model provided a modification
-        if modified_html:
-            st.text_area("Modified HTML:", modified_html, height=300)
-            st.download_button("Download Modified HTML", modified_html, "modified.html")
-        else:
-            st.error("No modifications were made. Please refine your instructions.")
+    # Display the modified HTML or an error message
+    if modified_html:
+        st.text_area("Modified HTML:", modified_html, height=300)
+        st.download_button("Download Modified HTML", modified_html, "modified.html")
+    else:
+        st.error("No modifications were made. Please refine your instructions.")
