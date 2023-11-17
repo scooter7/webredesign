926ab65a4c526e2bbfca91d72448d3ccc350e318
scooter7 <scooter.vineburgh@gmail.com>
2023-11-16 22:04:37 -0600

    Update webredesign.py

--- webredesign.py
+++ webredesign.py
@@ -24,31 +24,43 @@ modification_request = st.text_area("Describe the modifications you want:")
 if modification_request and soup:
     openai.api_key = st.secrets["OPENAI_API_KEY"]
     detailed_prompt = (
-        "Generate complete and functional HTML/CSS code for the following modification: "
-        f"'{modification_request}'."
+        f"Here is the HTML source code:\n{soup.prettify()}\n\n"
+        f"Make the following changes to the HTML source code based on these instructions:\n{modification_request}\n\n"
+        "Modified HTML:"
     )
     response = openai.completions.create(
         model="text-davinci-003",  # Replace with the correct GPT-4 model identifier
         prompt=detailed_prompt,
-        max_tokens=150
+        max_tokens=1000
     )
 
-    generated_code = response.choices[0].text.strip()
-    st.text_area("Generated HTML/CSS", generated_code)
+    modified_html = response.choices[0].text.strip()
+    if modified_html:
+        st.text_area("Modified HTML/CSS", modified_html, height=300)
 
-    # Merging the generated code with the original HTML
-    if "<style>" in generated_code or "css" in modification_request.lower():
-        style_tag = soup.new_tag("style")
-        style_tag.string = generated_code
-        soup.head.append(style_tag)
+        # Parse the modified HTML
+        modified_soup = BeautifulSoup(modified_html, 'html.parser')
+
+        # Merging the generated code with the original HTML
+        # Implementing a more sophisticated logic here based on the nature of modifications
+        # This can include checking for new styles, structural changes, new elements, etc.
+
+        # Example: If new styles are added, append them to the head
+        for style in modified_soup.find_all("style"):
+            soup.head.append(style)
+
+        # Example: If new body content is added, insert it
+        new_body = modified_soup.body
+        if new_body:
+            soup.body.replace_with(new_body)
+
+        # Final modified HTML
+        final_modified_html = soup.prettify()
+        st.text_area("Final Modified Source Code", final_modified_html, height=300)
+
+        # Providing a download link for the modified HTML
+        b64 = base64.b64encode(final_modified_html.encode()).decode()
+        href = f'<a href="data:file/html;base64,{b64}" download="modified_page.html">Download Modified HTML</a>'
+        st.markdown(href, unsafe_allow_html=True)
     else:
-        new_content = BeautifulSoup(generated_code, 'html.parser')
-        soup.body.insert(0, new_content)
-
-    modified_html = soup.prettify()
-    st.text_area("Modified Source Code", modified_html, height=300)
-
-    # Providing a download link for the modified HTML
-    b64 = base64.b64encode(modified_html.encode()).decode()
-    href = f'<a href="data:file/html;base64,{b64}" download="modified_page.html">Download Modified HTML</a>'
-    st.markdown(href, unsafe_allow_html=True)
+        st.error("No modifications were made. Please refine your instructions.")
