dacb4a7b187e19f2d93d5f332eb5d2b3280026af
scooter7 <scooter.vineburgh@gmail.com>
2023-11-16 19:40:29 -0600

    Update webredesign.py

--- webredesign.py
+++ webredesign.py
@@ -4,38 +4,41 @@ from bs4 import BeautifulSoup
 import openai
 import base64
 
-# Initialize Streamlit app
 st.title("Web Page Modifier")
 
-# Input fields for URL and modifications
 url = st.text_input("Enter the URL of the web page:")
 modification_request = st.text_area("Describe the modifications you want:")
 
 if url:
-    # Fetch and display web page source
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     st.text_area("Source Code", soup.prettify(), height=300)
 
 if modification_request:
-    # Use OpenAI API for natural language processing
+    if 'messages' not in st.session_state:
+        st.session_state.messages = []
+
+    user_message = {"role": "user", "content": f"Translate this into HTML/CSS: {modification_request}"}
+    st.session_state.messages.append(user_message)
+
     openai.api_key = st.secrets["OPENAI_API_KEY"]
-    response = openai.Completion.create(
+    response = openai.chat.completions.create(
         model="text-davinci-003",
-        prompt=f"Translate this into HTML/CSS: {modification_request}",
-        max_tokens=150
+        messages=st.session_state.messages
     )
-    generated_code = response.choices[0].text.strip()
-    st.text_area("Generated Code", generated_code)
 
-    # Apply and display modifications (simplified example)
-    new_style = soup.new_tag('style')
-    new_style.string = generated_code
-    soup.head.append(new_style)
-    modified_html = soup.prettify()
-    st.text_area("Modified Source Code", modified_html, height=300)
+    response_content = response.choices[0].message['content']
+    st.session_state.messages.append({"role": "assistant", "content": response_content})
 
-    # Create downloadable file
-    b64 = base64.b64encode(modified_html.encode()).decode()
-    href = f'<a href="data:file/html;base64,{b64}" download="modified_page.html">Download Modified HTML</a>'
-    st.markdown(href, unsafe_allow_html=True)
+    st.text_area("Generated HTML/CSS", response_content)
+
+    if soup:
+        new_style = soup.new_tag('style')
+        new_style.string = response_content
+        soup.head.append(new_style)
+        modified_html = soup.prettify()
+        st.text_area("Modified Source Code", modified_html, height=300)
+
+        b64 = base64.b64encode(modified_html.encode()).decode()
+        href = f'<a href="data:file/html;base64,{b64}" download="modified_page.html">Download Modified HTML</a>'
+        st.markdown(href, unsafe_allow_html=True)
