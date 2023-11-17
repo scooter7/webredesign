e5d9f4dbd7f2f1c1e9895428e3de2342651fc8c5
scooter7 <scooter.vineburgh@gmail.com>
2023-11-16 21:01:08 -0600

    Update webredesign.py

--- webredesign.py
+++ webredesign.py
@@ -15,16 +15,10 @@ if url:
     st.text_area("Source Code", soup.prettify(), height=300)
 
 if modification_request:
-    detailed_prompt = (
-        "Translate the following web design modification request into HTML and CSS code.\n"
-        f"Request: '{modification_request}'\n"
-        "Provide the complete HTML and CSS code necessary to implement this modification."
-    )
-
     openai.api_key = st.secrets["OPENAI_API_KEY"]
     response = openai.completions.create(
         model="text-davinci-003",
-        prompt=detailed_prompt,
+        prompt=f"Translate this into HTML/CSS: {modification_request}",
         max_tokens=150
     )
 
@@ -32,9 +26,12 @@ if modification_request:
     st.text_area("Generated HTML/CSS", response_content)
 
     if soup:
+        # Assuming the response_content is CSS, you can create a new <style> tag and append it
+        # If it's HTML, you might need to parse it and integrate it appropriately
         new_style = soup.new_tag('style')
         new_style.string = response_content
         soup.head.append(new_style)
+
         modified_html = soup.prettify()
         st.text_area("Modified Source Code", modified_html, height=300)
 
