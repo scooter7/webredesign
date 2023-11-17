59ece4b6710d89e7d7ec4de00392b02c9f5d72c8
scooter7 <scooter.vineburgh@gmail.com>
2023-11-16 21:56:42 -0600

    Update webredesign.py

--- webredesign.py
+++ webredesign.py
@@ -20,23 +20,17 @@ instructions = st.text_area("Enter your modification instructions:", height=100)
 modify_button = st.button("Modify Webpage")
 
 if modify_button and instructions and html_content:
-    # Structuring the prompt for OpenAI in a more directive manner
     prompt = f"Given the HTML code:\n\n{html_content}\n\nApply these changes: {instructions}\n\nThe modified HTML should look like this:"
+    response = openai.completions.create(
+        model="text-davinci-004", 
+        prompt=prompt, 
+        max_tokens=1000
+    )
 
-    # Process instructions with OpenAI
-    try:
-        response = openai.completions.create(
-            model="text-davinci-004", 
-            prompt=prompt, 
-            max_tokens=1000
-        )
-        modified_html = response.choices[0].text.strip()
+    generated_code = response.choices[0].text.strip()
 
-        # Check if the response is valid and display it
-        if modified_html and modified_html != html_content:
-            st.text_area("Modified HTML:", modified_html, height=300)
-            st.download_button("Download Modified HTML", modified_html, "modified.html")
-        else:
-            st.warning("The model did not return any modifications. Please refine your instructions.")
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+    if generated_code and generated_code != html_content:
+        st.text_area("Generated HTML/CSS", generated_code, height=300)
+        st.download_button("Download Modified HTML", generated_code, "modified.html")
+    else:
+        st.warning("The model did not return any modifications. Please refine your instructions.")
