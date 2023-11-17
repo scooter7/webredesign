d4985a765cbcb21091f4cd0b8777c306aebff132
scooter7 <scooter.vineburgh@gmail.com>
2023-11-16 22:35:39 -0600

    Update webredesign.py

--- webredesign.py
+++ webredesign.py
@@ -4,38 +4,44 @@ from bs4 import BeautifulSoup
 import openai
 import base64
 
+def chunk_string(string, length):
+    for i in range(0, len(string), length):
+        yield string[i:i + length]
+
+def get_modifications(chunk, instructions, api_key):
+    openai.api_key = api_key
+    prompt = f"Modify this HTML chunk based on these instructions:\nInstructions: {instructions}\nHTML Chunk:\n{chunk}"
+    response = openai.completions.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1000
+    )
+    return response.choices[0].text.strip()
+
 st.title("Web Page Modifier")
 
 url = st.text_input("Enter the URL of the web page:")
 modification_request = st.text_area("Describe the modifications you want in natural language:")
 
-if url:
+if url and modification_request:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     full_html_content = soup.prettify()
     st.text_area("Original HTML", full_html_content, height=300)
 
-if url and modification_request:
-    openai.api_key = st.secrets["OPENAI_API_KEY"]
-    
-    # Since processing the entire HTML might exceed token limits,
-    # we prioritize sections most likely to be relevant for modification.
-    # For example, we could focus on the first 2000 characters of the HTML.
-    # You can adjust this based on your specific needs.
-    limited_html_content = full_html_content[:2000]
+    # Retrieve the OpenAI API key from Streamlit secrets
+    api_key = st.secrets["OPENAI_API_KEY"]
 
-    prompt = f"Here is a portion of the HTML source code:\n{limited_html_content}\n\nApply the following changes based on these instructions:\n{modification_request}"
-    response = openai.completions.create(
-        model="text-davinci-003",  # Use the correct GPT model identifier
-        prompt=prompt,
-        max_tokens=1000  # Adjust based on your needs
-    )
+    html_chunks = list(chunk_string(full_html_content, 2000))
+    modified_html_chunks = []
 
-    modified_html = response.choices[0].text.strip()
+    for chunk in html_chunks:
+        modified_chunk = get_modifications(chunk, modification_request, api_key)
+        modified_html_chunks.append(modified_chunk)
+
+    final_modified_html = ''.join(modified_html_chunks)
+    st.text_area("Modified HTML", final_modified_html, height=300)
     
-    if modified_html:
-        st.text_area("Modified HTML", modified_html, height=300)
-        
-        b64 = base64.b64encode(modified_html.encode()).decode()
-        href = f'<a href="data:file/html;base64,{b64}" download="modified_page.html">Download Modified HTML</a>'
-        st.markdown(href, unsafe_allow_html=True)
+    b64 = base64.b64encode(final_modified_html.encode()).decode()
+    href = f'<a href="data:file/html;base64,{b64}" download="modified_page.html">Download Modified HTML</a>'
+    st.markdown(href, unsafe_allow_html=True)
