e334da5129152a90c7f2700bd10f18f9bbfd5d1b
scooter7 <scooter.vineburgh@gmail.com>
2023-11-16 22:17:31 -0600

    Update webredesign.py

--- webredesign.py
+++ webredesign.py
@@ -4,13 +4,13 @@ from bs4 import BeautifulSoup
 import openai
 import base64
 
-def get_modifications(html, instructions, api_key):
+def get_modifications(html_snippet, instructions, api_key):
     openai.api_key = api_key
-    prompt = f"Modify the following HTML based on these instructions:\n{instructions}\nHTML:\n{html}"
+    prompt = f"Modify the following HTML snippet based on these instructions:\nInstructions: {instructions}\nHTML Snippet:\n{html_snippet}"
     response = openai.completions.create(
         model="text-davinci-003",  # Use the correct GPT model identifier
         prompt=prompt,
-        max_tokens=500
+        max_tokens=500  # Adjust based on your needs
     )
     return response.choices[0].text.strip()
 
@@ -24,11 +24,14 @@ if url and modification_request:
     soup = BeautifulSoup(response.content, 'html.parser')
     st.text_area("Original HTML", soup.prettify(), height=300)
 
-    modified_html = get_modifications(soup.prettify(), modification_request, st.secrets["OPENAI_API_KEY"])
-    
-    if modified_html:
-        st.text_area("Modified HTML", modified_html, height=300)
+    html_snippet = st.text_area("Enter the HTML snippet you want to modify:", height=150)
+
+    if html_snippet:
+        modified_html = get_modifications(html_snippet, modification_request, st.secrets["OPENAI_API_KEY"])
         
-        b64 = base64.b64encode(modified_html.encode()).decode()
-        href = f'<a href="data:file/html;base64,{b64}" download="modified_page.html">Download Modified HTML</a>'
-        st.markdown(href, unsafe_allow_html=True)
+        if modified_html:
+            st.text_area("Modified HTML", modified_html, height=300)
+            
+            b64 = base64.b64encode(modified_html.encode()).decode()
+            href = f'<a href="data:file/html;base64,{b64}" download="modified_page.html">Download Modified HTML</a>'
+            st.markdown(href, unsafe_allow_html=True)
