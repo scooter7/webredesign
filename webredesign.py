5c38d55ab14f5999961186bdf4bb14ceaa51621b
scooter7 <scooter.vineburgh@gmail.com>
2023-11-16 20:13:05 -0600

    Update webredesign.py

--- webredesign.py
+++ webredesign.py
@@ -15,21 +15,14 @@ if url:
     st.text_area("Source Code", soup.prettify(), height=300)
 
 if modification_request:
-    if 'messages' not in st.session_state:
-        st.session_state.messages = []
-
-    user_message = {"role": "user", "content": f"Translate this into HTML/CSS: {modification_request}"}
-    st.session_state.messages.append(user_message)
-
     openai.api_key = st.secrets["OPENAI_API_KEY"]
     response = openai.completions.create(
         model="text-davinci-003",
-        messages=st.session_state.messages
+        prompt=f"Translate this into HTML/CSS: {modification_request}",
+        max_tokens=150
     )
 
-    response_content = response.choices[0].message['content']
-    st.session_state.messages.append({"role": "assistant", "content": response_content})
-
+    response_content = response.choices[0].text.strip()
     st.text_area("Generated HTML/CSS", response_content)
 
     if soup:
