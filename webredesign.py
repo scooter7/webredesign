4f47869a069a1d40b72e947fb001af7b046b58f7
scooter7 <scooter.vineburgh@gmail.com>
2023-11-16 22:21:04 -0600

    Update webredesign.py

--- webredesign.py
+++ webredesign.py
@@ -4,34 +4,31 @@ from bs4 import BeautifulSoup
 import openai
 import base64
 
-def get_modifications(html_snippet, instructions, api_key):
-    openai.api_key = api_key
-    prompt = f"Modify the following HTML snippet based on these instructions:\nInstructions: {instructions}\nHTML Snippet:\n{html_snippet}"
-    response = openai.completions.create(
-        model="text-davinci-003",  # Use the correct GPT model identifier
-        prompt=prompt,
-        max_tokens=500  # Adjust based on your needs
-    )
-    return response.choices[0].text.strip()
-
 st.title("Web Page Modifier")
 
 url = st.text_input("Enter the URL of the web page:")
-modification_request = st.text_area("Describe the modifications you want:")
+modification_request = st.text_area("Describe the modifications you want in natural language:")
 
 if url and modification_request:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     st.text_area("Original HTML", soup.prettify(), height=300)
 
-    html_snippet = st.text_area("Enter the HTML snippet you want to modify:", height=150)
+    html_content = soup.prettify()[:2000]  # Adjust as needed to fit within token limits
 
-    if html_snippet:
-        modified_html = get_modifications(html_snippet, modification_request, st.secrets["OPENAI_API_KEY"])
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
+    prompt = f"Here is the HTML source code:\n{html_content}\n\nApply the following changes based on these instructions:\n{modification_request}"
+    response = openai.completions.create(
+        model="text-davinci-003",  # Use the correct GPT model identifier
+        prompt=prompt,
+        max_tokens=1000  # Adjust based on your needs
+    )
+
+    modified_html = response.choices[0].text.strip()
+    
+    if modified_html:
+        st.text_area("Modified HTML", modified_html, height=300)
         
-        if modified_html:
-            st.text_area("Modified HTML", modified_html, height=300)
-            
-            b64 = base64.b64encode(modified_html.encode()).decode()
-            href = f'<a href="data:file/html;base64,{b64}" download="modified_page.html">Download Modified HTML</a>'
-            st.markdown(href, unsafe_allow_html=True)
+        b64 = base64.b64encode(modified_html.encode()).decode()
+        href = f'<a href="data:file/html;base64,{b64}" download="modified_page.html">Download Modified HTML</a>'
+        st.markdown(href, unsafe_allow_html=True)
